ad508691dc56c53bafbee696b051b547cea2c4a3
fishinthenorthsea <84213760@qq.com>
2022-09-26 21:01:33 +0800

    asio

--- source/main.cpp
+++ source/main.cpp
@@ -3,48 +3,126 @@
 #include "utils/timestamp.h"
 #include "utils/mutex.h"
 #include "spdlog/spdlog.h"
-//#include "spdlog/spdlog.h"
-uint32 num = 0;
-Mutex mutex;
+#include <array>
+#include <iostream>
+#include <memory>
+#include <asio/asio.hpp>
+#include <asio/asio/experimental/as_tuple.hpp>
+#include <asio/asio/experimental/awaitable_operators.hpp>
 
-void ttt(){
-    RETURN_VOID;
+using asio::awaitable;
+using asio::buffer;
+using asio::co_spawn;
+using asio::detached;
+using asio::ip::tcp;
+namespace this_coro = asio::this_coro;
+using namespace asio::experimental::awaitable_operators;
+using std::chrono::steady_clock;
+using namespace std::literals::chrono_literals;
+
+constexpr auto use_nothrow_awaitable = asio::experimental::as_tuple(asio::use_awaitable);
+
+awaitable<void> timeout(steady_clock::duration duration)
+{
+  asio::steady_timer timer(co_await this_coro::executor);
+  timer.expires_after(duration);
+  co_await timer.async_wait(use_nothrow_awaitable);
 }
-using namespace kw;
 
+awaitable<void> transfer(tcp::socket& from, tcp::socket& to)
+{
+  std::array<char, 1024> data;
 
-int main(){
-    std::cout<<timeStamp::NowSecond()<<std::endl;
-    std::cout<<timeStamp::NowMs()<<std::endl;
-    std::cout<<timeStamp::formatString()<<std::endl;
+  for (;;)
+  {
+    auto result1 = co_await (
+        from.async_read_some(buffer(data), use_nothrow_awaitable) ||
+        timeout(5s)
+      );
 
-    std::cout<<"123";
-    std::cout<<va("wudi %d  %d!\n",5,100);
+    if (result1.index() == 1)
+      co_return; // timed out
 
-    std::string sss("12345");
-    LogInfo("sss\n");
-    ttt();
-    
+    auto [e1, n1] = std::get<0>(result1);
+    if (e1)
+      break;
 
-   
+    auto result2 = co_await (
+        async_write(to, buffer(data, n1), use_nothrow_awaitable) ||
+        timeout(1s)
+      );
 
+    if (result2.index() == 1)
+      co_return; // timed out
 
-    // std::thread a([&](){
-    //     for(auto i = 0 ; i < 10000000 ; ++i){           
-    //         mutex.lock();
-    //         ++num;
-    //         mutex.unlock();
-    //     }
-    // });
+    auto [e2, n2] = std::get<0>(result2);
+    if (e2)
+      break;
+  }
+}
 
+awaitable<void> proxy(tcp::socket client, tcp::endpoint target)
+{
+  tcp::socket server(client.get_executor());
 
-    // for(auto i = 0 ; i < 10000000 ; ++i){
-    //         mutex.lock();
-    //         ++num;
-    //         mutex.unlock();
-    // }
-    // a.join();
+  auto [e] = co_await server.async_connect(target, use_nothrow_awaitable);
+  if (!e)
+  {
+    co_await (
+        transfer(client, server) ||
+        transfer(server, client)
+      );
+  }
+}
 
-    // std::cout<<num<<std::endl;
-    return 0;
+awaitable<void> listen(tcp::acceptor& acceptor, tcp::endpoint target)
+{
+  for (;;)
+  {
+    auto [e, client] = co_await acceptor.async_accept(use_nothrow_awaitable);
+    if (e)
+      break;
+
+    auto ex = client.get_executor();
+    co_spawn(ex, proxy(std::move(client), target), detached);
+  }
+}
+
+int main(int argc, char* argv[])
+{
+  try
+  {
+    if (argc != 5)
+    {
+      std::cerr << "Usage: proxy";
+      std::cerr << " <listen_address> <listen_port>";
+      std::cerr << " <target_address> <target_port>\n";
+      return 1;
+    }
+
+    asio::io_context ctx;
+
+    auto listen_endpoint =
+      *tcp::resolver(ctx).resolve(
+          argv[1],
+          argv[2],
+          tcp::resolver::passive
+        );
+
+    auto target_endpoint =
+      *tcp::resolver(ctx).resolve(
+          argv[3],
+          argv[4]
+        );
+
+    tcp::acceptor acceptor(ctx, listen_endpoint);
+
+    co_spawn(ctx, listen(acceptor, target_endpoint), detached);
+
+    ctx.run();
+  }
+  catch (std::exception& e)
+  {
+    std::cerr << "Exception: " << e.what() << "\n";
+  }
 }
